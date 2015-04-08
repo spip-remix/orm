bfa111b9acc83c2316d41067937b619e14993fb4
Matthieu Marcillaud <marcimat@rezo.net>
2015-04-08 19:19:30 +0000

    Notice PHP en moins.

--- base/abstract_sql.php
+++ base/abstract_sql.php
@@ -49,7 +49,7 @@ function sql_error_backtrace($compil_info = false){
 		);
 		return $contexte_compil;
 	}
-	$message = $trace[0]['file']." L".$trace[0]['line'];
+	$message = count($trace) ? $trace[0]['file']." L".$trace[0]['line'] : "";
 	$f = array();
 	while(count($trace) AND $t=array_shift($trace)){
 		if (in_array($t['function'],array('include_once','include_spip','find_in_path')))
