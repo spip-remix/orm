ad53cca6c10cced4a4966b42d1f9bbff15d5a14f
Cerdic <cedric@yterium.com>
2021-09-27 19:00:41 +0200

    Complement de 57572165f6 : _mysql_traite_query() s'appelle recursivement, elle ne doit echapper les textes qu'au premier appel, car ensuite ce n'est plus necessaire et elle risquerait potentiellement de melanger les pourcents de substitutions/remplacement dans query_reinjecte_textes()

--- req/mysql.php
+++ req/mysql.php
@@ -517,9 +517,10 @@ define('_SQL_PREFIXE_TABLE_MYSQL', '/([,\s])spip_/S');
  * @param string $query Requête à préparer
  * @param string $db Nom de la base de donnée
  * @param string $prefixe Préfixe de tables à appliquer
+ * @param bool $echappe_textes Pour ne pas essayer de re-echapper une chaine deja echappee qu'on traite en recursif
  * @return string           Requête préparée
  */
-function _mysql_traite_query($query, $db = '', $prefixe = '') {
+function _mysql_traite_query($query, $db = '', $prefixe = '', $echappe_textes = true) {
 
 	if ($GLOBALS['mysql_rappel_nom_base'] and $db) {
 		$pref = '`' . $db . '`.';
@@ -540,10 +541,20 @@ function _mysql_traite_query($query, $db = '', $prefixe = '') {
 		// il faut alors echapper les chaine avant de le faire, pour ne pas risquer de
 		// modifier une requete qui est en fait juste du texte dans un champ
 		if (stripos($suite, 'SELECT') !== false) {
-			list($suite_echap, $textes) = query_echappe_textes($suite);
+			if ($echappe_textes) {
+				list($suite_echap, $textes) = query_echappe_textes($suite);
+			}
+			else {
+				$suite_echap = $suite;
+			}
 			if (preg_match('/^(.*?)([(]\s*SELECT\b.*)$/si', $suite_echap, $r)) {
-				$suite_echap = $r[1] . _mysql_traite_query($r[2], $db, $prefixe);
-				$suite = query_reinjecte_textes($suite_echap, $textes);
+				$suite_echap = $r[1] . _mysql_traite_query($r[2], $db, $prefixe, false);
+				if ($echappe_textes) {
+					$suite = query_reinjecte_textes($suite_echap, $textes);
+				}
+				else {
+					$suite = $suite_echap;
+				}
 			}
 		}
 	}
