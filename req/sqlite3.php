ff5405495aa873dd4d06fd5cf31097bde37778d9
Matthieu Marcillaud <marcimat@rezo.net>
2023-01-08 18:49:00 +0100

    docs: Nettoyer les entêtes de fichiers de la mention du fichier COPYING.txt
    Refs: #5369

--- req/sqlite3.php
+++ req/sqlite3.php
@@ -7,7 +7,6 @@
  *  Arnaud Martin, Antoine Pitrou, Philippe Rivière, Emmanuel Saint-James  *
  *                                                                         *
  *  Ce programme est un logiciel libre distribué sous licence GNU/GPL.     *
- *  Pour plus de détails voir le fichier COPYING.txt ou l'aide en ligne.   *
 \***************************************************************************/
 
 if (!defined('_ECRIRE_INC_VERSION')) {
